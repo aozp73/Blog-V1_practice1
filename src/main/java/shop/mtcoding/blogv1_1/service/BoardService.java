32db768d1750c3051130870812a774ceae04cd1b
aozp73 <aozp73@naver.com>
2023-02-11 15:44:28 +0900

    글쓰기 Controller 완료

--- src/main/java/shop/mtcoding/blogv1_1/service/BoardService.java
+++ src/main/java/shop/mtcoding/blogv1_1/service/BoardService.java
@@ -2,7 +2,13 @@ package shop.mtcoding.blogv1_1.service;
 
 import org.springframework.stereotype.Service;
 
+import shop.mtcoding.blogv1_1.dto.board.boardReq.BoardSaveReqDto;
+
 @Service
 public class BoardService {
+
+    public void 게시글작성(BoardSaveReqDto boardSaveReqDto) {
+
+    }
 
 }
