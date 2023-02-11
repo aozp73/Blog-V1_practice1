47de853e9388b96658b0db7f697afa6bdd49e581
aozp73 <122352251+aozp73@users.noreply.github.com>
2023-02-11 17:27:55 +0900

    Board main get topic (#7)

--- src/main/java/shop/mtcoding/blogv1_1/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blogv1_1/controller/BoardController.java
@@ -2,13 +2,19 @@ package shop.mtcoding.blogv1_1.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.blogv1_1.dto.ResponseDto;
 import shop.mtcoding.blogv1_1.dto.board.boardReq.BoardSaveReqDto;
-import shop.mtcoding.blogv1_1.handler.ex.CustomException;
+import shop.mtcoding.blogv1_1.handler.ex.CustomApiException;
+import shop.mtcoding.blogv1_1.model.BoardRepository;
 import shop.mtcoding.blogv1_1.model.User;
 import shop.mtcoding.blogv1_1.service.BoardService;
 
@@ -18,38 +24,41 @@ public class BoardController {
 
     private final HttpSession session;
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
+
+    @PostMapping("/board") // 유효성 검사(Post), 인증 o, 권한 x
+    public ResponseEntity<?> save(@RequestBody BoardSaveReqDto boardSaveReqDto) {
+        // 인증
+        System.out.println("테스트1" + boardSaveReqDto.getTitle());
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomApiException("로그인이 필요합니다");
+        }
+
+        // 유효성 검사
+        if (boardSaveReqDto.getTitle() == null || boardSaveReqDto.getTitle().isEmpty()) {
+            throw new CustomApiException("제목을 입력하세요");
+        }
+        if (boardSaveReqDto.getContent() == null || boardSaveReqDto.getContent().isEmpty()) {
+            throw new CustomApiException("내용을 입력하세요");
+        }
+        if (boardSaveReqDto.getTitle().length() > 100) {
+            throw new CustomApiException("제목은 100자 이하여야 합니다");
+        }
+
+        boardService.게시글작성(boardSaveReqDto, principal.getId());
+
+        return new ResponseEntity<>(new ResponseDto<>(1, "게시글 등록완료", null), HttpStatus.CREATED);
+    }
 
     @GetMapping({ "/", "/main" })
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("dtos", boardRepository.findAllWithUsername());
         return "/board/main";
     }
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "/board/saveForm";
-    }
-
-    @PostMapping("/board") // 유효성 검사(Post), 인증 o, 권한 x
-    public String save(BoardSaveReqDto boardSaveReqDto) {
-        // 인증
-        User principal = (User) session.getAttribute("principal");
-        if (principal == null) {
-            throw new CustomException("로그인이 필요합니다");
-        }
-
-        // 유효성 검사
-        if (boardSaveReqDto.getTitle() == null || boardSaveReqDto.getTitle().isEmpty()) {
-            throw new CustomException("제목을 입력하세요");
-        }
-        if (boardSaveReqDto.getContent() == null || boardSaveReqDto.getContent().isEmpty()) {
-            throw new CustomException("내용을 입력하세요");
-        }
-        if (boardSaveReqDto.getTitle().length() > 100) {
-            throw new CustomException("제목은 100자 이하여야 합니다");
-        }
-
-        boardService.게시글작성(boardSaveReqDto, principal.getId());
-
-        return "redirect:/";
     }
 }
---
+++ src/test/java/shop/mtcoding/blogv1_1/controller/model/BoardRepositoryTest.java
@@ -0,0 +1,38 @@
+package shop.mtcoding.blogv1_1.controller.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.blogv1_1.dto.board.boardResp.BoardMainRespDto;
+import shop.mtcoding.blogv1_1.model.BoardRepository;
+
+@MybatisTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    ObjectMapper om;
+
+    @Test
+    public void findAllWithUser_test() throws Exception {
+        // given
+        om = new ObjectMapper();
+
+        // when
+        List<BoardMainRespDto> boardMainRespDto = boardRepository.findAllWithUsername();
+        // String responseBody = om.writeValueAsString(boardMainRespDto);
+        // System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(boardMainRespDto.get(0).getContent()).isEqualTo("1번째 내용");
+
+    }
+}
