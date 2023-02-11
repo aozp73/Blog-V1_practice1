b49834acaabd68c11fe3e7ca23898202fe061dec
aozp73 <aozp73@naver.com>
2023-02-11 16:01:37 +0900

    글쓰기 Controller test 완료

---
+++ src/test/java/shop/mtcoding/blogv1_1/controller/BoardControllerTest.java
@@ -0,0 +1,63 @@
+package shop.mtcoding.blogv1_1.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.blogv1_1.model.User;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class BoardControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    private MockHttpSession mockSession;
+
+    @BeforeEach
+    public void setUp() {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("ssar");
+        user.setPassword("1234");
+        user.setEmail("ssar@nate.com");
+        user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+
+        mockSession = new MockHttpSession();
+        mockSession.setAttribute("principal", user);
+    }
+
+    @Test
+    public void insert_test() throws Exception {
+        // given
+        String requestBody = "title=제목입니다&content=내용입니다";
+
+        // then
+
+        User user = (User) mockSession.getAttribute("principal");
+        System.out.println(user.getUsername());
+        ResultActions resultActions = mvc.perform(post("/board")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .session(mockSession));
+
+        // when
+        resultActions.andExpect(status().isOk());
+    }
+}
