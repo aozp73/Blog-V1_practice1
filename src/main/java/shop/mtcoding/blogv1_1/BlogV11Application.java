0fc3b68d065147d75bb13bf2c7b08b9595a1238b
aozp73 <aozp73@naver.com>
2023-02-11 10:04:31 +0900

    프로젝트 환경세팅

---
+++ src/main/java/shop/mtcoding/blogv1_1/BlogV11Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blogv1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogV11Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/blogv1_1/BlogV11ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blogv1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
