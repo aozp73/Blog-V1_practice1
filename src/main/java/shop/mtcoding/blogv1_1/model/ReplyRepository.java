a30344f0943451295c9a931b8217a51889aa350e
aozp73 <aozp73@naver.com>
2023-02-11 20:42:32 +0900

    댓글보기 테이블 모델링, mapper 완료

---
+++ src/main/java/shop/mtcoding/blogv1_1/model/ReplyRepository.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.blogv1_1.model;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ReplyRepository {
+    public List<Reply> findAll();
+
+    public Reply findById(int id);
+
+    public int insert(@Param("comment") String comment, @Param("boardId") int boardId, @Param("userId") int userId);
+
+    public int updateById(@Param("id") int id, @Param("comment") String comment);
+
+    public int deleteById(int id);
+}
