6b12975fb3ab9ed8369545149afbe6ff99c85c16
aozp73 <aozp73@naver.com>
2023-02-11 15:35:56 +0900

    기타) 로그아웃, 헤더부분 반영 완료

--- src/main/java/shop/mtcoding/blogv1_1/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blogv1_1/controller/BoardController.java
@@ -1,8 +1,18 @@
 package shop.mtcoding.blogv1_1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @GetMapping({ "/", "/main" })
+    public String main() {
+        return "/board/main";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "/board/saveForm";
+    }
 }
